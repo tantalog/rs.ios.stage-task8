987052c2eaf7938fb7cbdc3bfa275928f32c0c04
tantalog <kushnerovanton@gmail.com>
2021-07-15 23:15:02 +0300

    init: project created

---
+++ RSSchool_T8/RSSchool_T8/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  RSSchool_T8
+//
+//  Created by Anton Kushnerov on 15.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RSSchool_T8/RSSchool_T8/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RSSchool_T8
+//
+//  Created by Anton Kushnerov on 15.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
